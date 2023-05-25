facb722f666a4b2c00e8817d87b778b87e274d87
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-05-25 10:34:11 +0200

    commit

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,33 +1,56 @@
+class Nodo:
+    def __init__(self, fila):
+        self.fila = fila
+        self.siguiente = None
+
+
 class Grafo:
     def __init__(self, n):
         self.n = n
-        self.posiciones = [None] * n
+        self.nodos = [None] * n
+
+    def agregar_nodo(self, fila):
+        nodo = Nodo(fila)
+        if not self.nodos[fila]:
+            self.nodos[fila] = nodo
+        else:
+            actual = self.nodos[fila]
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nodo
+
 
     def es_valido(self, fila, columna):
-        for i in range(self.n):
-            if self.posiciones[i] is not None:
-                if self.posiciones[i][1] == columna:  # Verificar misma columna
-                    return False
-                if abs(self.posiciones[i][0] - fila) == abs(self.posiciones[i][1] - columna):  # Verificar diagonales
-                    return False
+        actual= self.nodos[fila]
+        while actual:
+            if actual.fila == columna:
+                return False
+            if abs(actual.fila - columna) == abs(fila - 1):
+                return False
+            actual = actual.siguiente
         return True
 
     def backtrack(self, fila):
-        if fila == self.n:
-            return True
-        for columna in range(self.n):
-            if self.es_valido(fila, columna):
-                self.posiciones[fila] = (fila, columna)
-                if self.backtrack(fila + 1):
-                    return True
-                self.posiciones[fila] = None
-        return False
+            if fila == self.n:
+                solucion = []
+                for i in range(self.n):
+                    actual = self.nodos[i]
+                    while actual:
+                        solucion.append(actual.fila)
+                        actual = actual.siguiente
+                return solucion
+            else:
+                for columna in range(self.n):
+                    if self.es_valido(fila, columna):
+                        self.agregar_nodo(fila)
+                        resultado = self.backtrack(fila + 1)
+                        if resultado:
+                            return resultado
+                        self.nodos[fila] = self.nodos[fila].siguiente
+            return None
 
     def encontrar_solucion(self):
-        if self.backtrack(0):
-            return self.posiciones
-        return None
-
+        return self.backtrack(0)
 
 def imprimir_tabla():
     print("{:<10s}{:<20s}".format("n", "Posición"))
