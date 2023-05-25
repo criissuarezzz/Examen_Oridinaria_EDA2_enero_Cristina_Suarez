c957fca646914376025c55700ac2b7122ac662f5
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-05-25 10:30:58 +0200

    una solucion

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,81 +1,49 @@
-class Nodo:
-    def __init__(self, fila):
-        self.fila = fila
-        self.siguiente = None
-
-
 class Grafo:
     def __init__(self, n):
         self.n = n
-        self.nodos = [None] * n
-        self.soluciones = []
-        self.solucion = []
-
-    def agregar_nodo(self, fila):
-        nodo = Nodo(fila)
-        if not self.nodos[fila]:
-            self.nodos[fila] = nodo
-        else:
-            actual = self.nodos[fila]
-            while actual.siguiente:
-                actual = actual.siguiente
-            actual.siguiente = nodo
+        self.posiciones = [None] * n
 
     def es_valido(self, fila, columna):
-        actual = self.nodos[fila]
-        while actual:
-            if actual.fila == columna:
-                return False
-            if abs(actual.fila - columna) == abs(fila - 1):
-                return False
-            actual = actual.siguiente
+        for i in range(self.n):
+            if self.posiciones[i] is not None:
+                if self.posiciones[i][1] == columna:  # Verificar misma columna
+                    return False
+                if abs(self.posiciones[i][0] - fila) == abs(self.posiciones[i][1] - columna):  # Verificar diagonales
+                    return False
         return True
 
     def backtrack(self, fila):
         if fila == self.n:
-            solucion = []
-            for i in range(self.n):
-                actual = self.nodos[i]
-                while actual:
-                    solucion.append(actual.fila)
-                    actual = actual.siguiente
-            self.soluciones.append(solucion)
-        else:
-            for columna in range(self.n):
-                if self.es_valido(fila, columna):
-                    self.agregar_nodo(fila)
-                    self.backtrack(fila + 1)
-                    self.nodos[fila] = self.nodos[fila].siguiente
-
-    def encontrar_soluciones(self):
-        self.backtrack(0)
-
-    def imprimir_soluciones(self):
-        for solucion in self.soluciones:
-            print(solucion)
+            return True
+        for columna in range(self.n):
+            if self.es_valido(fila, columna):
+                self.posiciones[fila] = (fila, columna)
+                if self.backtrack(fila + 1):
+                    return True
+                self.posiciones[fila] = None
+        return False
 
     def encontrar_solucion(self):
-        self.backtrack(0)
-        if self.soluciones:
-            self.solucion = self.soluciones[0]
-            return True
-        return False
-    
+        if self.backtrack(0):
+            return self.posiciones
+        return None
 
-# Imprimir tabla con la posición de una sola bola Pokémon por cada valor de n
-print("{:<10s}{:<20s}".format("n", "Posición"))
-for n in range(1, 11):
-    grafo = Grafo(n)
-    if grafo.encontrar_solucion():
-        posicion = str(grafo.solucion)
-    else:
-        posicion = "No hay solución"
-    print("{:<10d}{:<20s}".format(n, posicion))
 
-n = 15
-grafo = Grafo(n)
-if grafo.encontrar_solucion():
-    posicion = str(grafo.solucion)
-else:
-    posicion = "No hay solución"
-print("{:<10d}{:<20s}".format(n, posicion))
+def imprimir_tabla():
+    print("{:<10s}{:<20s}".format("n", "Posición"))
+    for n in range(1, 11):
+        grafo = Grafo(n)
+        solucion = grafo.encontrar_solucion()
+        if solucion:
+            posicion = str(solucion)
+        else:
+            posicion = "No hay solución"
+        print("{:<10d}{:<20s}".format(n, posicion))
+
+imprimir_tabla()
+
+#para n=15
+
+grafo=Grafo(15)
+solucion=grafo.encontrar_solucion()
+print(solucion)
